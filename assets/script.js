1db0fdefe34f37da4e4f19e7ff5292b9217da814
Michael Guidetti <mguidetti@users.noreply.github.com>
2024-08-12 12:37:19 -0400

    Use text presets

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,30 @@ class Resolume {
     this.baseUrl = `http://${address}:${port}/api/v1`;
   }
 
-  handleResponse(response) {
+  async handleResponse(response) {
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      const errorText = await response.text();
+      throw new Error(`Network response was not ok: ${errorText}`);
     } else {
-      console.log(response);
       return response;
     }
   }
 
-  async createTextBlock(layerIndex, clipIndex) {
+  async getSourcePresets(sourceName) {
+    const response = await fetch(`${this.baseUrl}/sources`);
+    const handledResponse = await this.handleResponse(response);
+    const data = await handledResponse.json();
+    const textBlockSource = data.video.find((obj) => obj.name === sourceName);
+    const presets = textBlockSource.presets;
+
+    return presets;
+  }
+
+  async createTextBlock(layerIndex, clipIndex, preset) {
+    const sourceName = `source:///video/Text Block${
+      preset !== "" ? `/${preset}` : ""
+    }`;
+
     const response = await fetch(
       `${this.baseUrl}/composition/layers/${layerIndex}/clips/${clipIndex}/open`,
       {
@@ -20,7 +34,7 @@ class Resolume {
         headers: {
           "Content-Type": "application/json",
         },
-        body: "source:///video/Text%20Block",
+        body: encodeURI(sourceName),
       }
     );
 
@@ -37,7 +51,6 @@ class Resolume {
           Text: {
             value: message,
           },
-          
         },
       },
     };
@@ -57,31 +70,56 @@ class Resolume {
   }
 }
 
+async function populatePresets(resolume, selectElement) {
+  selectElement.innerHTML = "";
+
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.text = "Default";
+  selectElement.appendChild(defaultOption);
+
+  const presets = await resolume.getSourcePresets("Text Block");
+  presets.forEach((preset) => {
+    const newOption = document.createElement("option");
+    newOption.value = preset.name;
+    newOption.text = preset.name;
+    selectElement.appendChild(newOption);
+  });
+}
+
 async function submitForm(event, resolume) {
   event.preventDefault();
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData.entries());
 
-  await resolume.createTextBlock(data.layer, data.clip);
+  await resolume.createTextBlock(data.layer, data.clip, data.preset);
   await resolume.updateTextBlock(data.layer, data.clip, data.message);
 
   const clipInput = document.getElementById("clip-input");
   const messageInput = document.getElementById("message-input");
 
-  clipInput.value++ 
-  messageInput.value = null
-  messageInput.focus()
+  clipInput.value++;
+  messageInput.value = null;
+  messageInput.focus();
 }
 
 function setup() {
   const address = document.getElementById("address-input").value;
   const port = document.getElementById("port-input").value;
+  const presetSelectInput = document.querySelector("select[name='preset']");
   const resolume = new Resolume(address, port);
+
+  populatePresets(resolume, presetSelectInput);
 
   document
     .getElementById("message-form")
     .addEventListener("submit", (event) => submitForm(event, resolume));
+  document
+    .getElementById("preset-refresh-button")
+    .addEventListener("click", () =>
+      populatePresets(resolume, presetSelectInput)
+    );
 }
 
 setup();
